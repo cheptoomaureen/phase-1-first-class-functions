50e3ac5ce7d23f82f8d67c148acb5ca6c092d7dc
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-03-22 22:15:11 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const chai = require("chai");
+const spies = require("chai-spies");
+chai.use(spies);
+function receivesAFunction(callback) {
+    callback();
+}
+function returnsANamedFunction() {
+    const fn = function() {
+      return "before all";
+    }
+    return fn;
+  }
+  function returnsAnAnonymousFunction() {
+    return function() {
+    }
+  }
+
